a3b2ab33f85829e5130a549b7048f3dfea4a939a
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-10-20 15:40:20 +0200

    Add css style,elements html and variables js

--- Assets/Js/main.js
+++ Assets/Js/main.js
@@ -4,3 +4,20 @@ con difficoltà 1 => tra 1 e 100
 con difficoltà 2 => tra 1 e 81
 con difficoltà 3 => tra 1 e 49
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro. */
+
+
+/* Strumenti
+-prompt
+-querySelector/getElementById
+-ciclo for
+-eventListener
+-if/else
+*/
+
+// inizializzazione delle variabili
+const levelOne = "1";
+const levelTwo = "2"; 
+const levelThree = "3";
+
+// selezionare elementi DOM
+const containerElement = document.querySelector(".container");
